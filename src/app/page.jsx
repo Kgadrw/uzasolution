938ac7f2853058183b0577d874b535fa9ff7bedb
Kgadrw <kalisagad05@gmail.com>
2025-04-25 20:24:33 +0200

    git fix vercel delpoyment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 'use client'
-import Hero from "../components/Hero"
-import AboutUs from "../components/Aboutus"
-import TrustedCompanies from "../components/Trustedco"
-import Profiles from "../components/Profiles"
-import '../app/globals.css'
+import Hero from "../../components/Hero"
+import AboutUs from "../../components/Aboutus"
+import TrustedCompanies from "../../components/Trustedco"
+import Profiles from "../../components/Profiles"
+import '../globals.css'
 
 import React from 'react'
 
@@ -18,4 +18,4 @@ const page = () => {
   )
 }
 
-export default page;
+export default page
